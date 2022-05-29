03619ebaa866741934c765a2ef5029f7f24b9fb7
Mohit Mishra <mohitvm2002@gmail.com>
2022-05-29 19:33:35 -0400

    Mohit Done

---
+++ bash/sysinfo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+Host Name : mohit21-virtual-machine #name of hostname command= hostname
+Domain Name: N/A #my pc does not have domain name
+Operating System name and version: #version and os of my machine command=hostnamectl
+          OS : Ubuntu 22.04 LTS             
+          Version: Linux 5.15.0-33-generic
+IP Addresses: # Ip address of my linux machine command = ip addr
+             inet: 192.168.139.130
+             inet6:fe80::51bf:51e9:c6b2:3193
+Space available in root filesystem # space available in my system command df-h
+Filesystem      Size  Used Avail Use% Mounted on
+/dev/sda3        20G  9.0G  9.1G  50% /
+
