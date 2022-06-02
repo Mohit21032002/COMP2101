a6bb20b53ce3bccf6e54b9d3373218db99b8ca2b
Mohit Mishra <mohitvm2002@gmail.com>
2022-06-02 12:15:03 -0400

    Mohit lab1 done

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-Host Name : mohit21-virtual-machine #name of hostname command= hostname
-Domain Name: N/A #my pc does not have domain name
-Operating System name and version: #version and os of my machine command=hostnamectl
-          OS : Ubuntu 22.04 LTS             
-          Version: Linux 5.15.0-33-generic
-IP Addresses: # Ip address of my linux machine command = ip addr
-             inet: 192.168.139.130
-             inet6:fe80::51bf:51e9:c6b2:3193
-Space available in root filesystem # space available in my system command df-h
-Filesystem      Size  Used Avail Use% Mounted on
-/dev/sda3        20G  9.0G  9.1G  50% /
+echo "Host Name : mohit21-virtual-machine" #name of hostname command= hostname
+echo "Domain Name: N/A" #my pc does not have domain name
+echo "Operating System name and version": #version and os of my machine command=hostnamectl
+echo          "OS : Ubuntu 22.04 LTS"             
+echo          "Version: Linux 5.15.0-33-generic"
+echo "IP Addresses:" # Ip address of my linux machine command = ip addr
+echo      "inet: 192.168.139.130"
+echo      "inet6:fe80::51bf:51e9:c6b2:3193"
+echo "Space available in root filesystem" # space available in my system command df-h
+echo "Filesystem      Size  Used Avail Use% Mounted on"
+echo "/dev/sda3        20G  9.0G  9.1G  50% /"
 
