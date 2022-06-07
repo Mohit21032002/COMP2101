eb8d568d9938f9bf76693f43e83fc8d74c0f2fd3
Mohit Mishra <mohitvm2002@gmail.com>
2022-06-07 17:56:09 -0400

    rolling

--- bash/improveddice.sh
+++ bash/improveddice.sh
@@ -3,14 +3,21 @@
 # this script rolls a pair of six-sided dice and displays both the rolls
 #
 
-# Task 1:
+echo "Task 1:"
 #  put the number of sides in a variable which is used as the range for the random number
 #  put the bias, or minimum value for the generated number in another variable
 #  roll the dice using the variables for the range and bias i.e. RANDOM % range + bias
+echo "Dice Rolling"
+mdie1=$(( RANDOM % 6 + 1 ))
+mdie2=$(( RANDOM % 6 + 1 ))
+echo "Rolled $mdie1, $mdie2"
 
-# Task 2:
+echo "Task 2:"
 #  generate the sum of the dice
 #  generate the average of the dice
+sum=$((mdie1 +mdie2))
+echo sum of rolled dices is $sum
+
 
 #  display a summary of what was rolled, and what the results of your arithmetic were
 
