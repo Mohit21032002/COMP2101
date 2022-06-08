ba161768a6482c8291fef6ab5a9b05be12d7bd78
Mohit Mishra <mohitvm2002@gmail.com>
2022-06-07 20:42:19 -0400

    sysinfo updated

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-#this command will show the hostname
-echo "Host Name:"
-hostname 
-echo "Domain Name:" #my pc does not have domain name
-domainname -d 
-echo "Operating System name and version:"
-grep PRETTY /etc/os-release #this will show the version of ubuntu               
-echo "IP Addresses:"
-hostname -I  # This will shoe the IP address of the system
-echo "Space available in root filesystem:"
-df -h /root # This command will show the avability of space in root filesystem
-exit
+
+echo Report for $HOSTNAME  #title
+echo ===================================
+echo "FQDN = $(hostname --fqdn)" #thsi command will show FQDN
+echo "Operating System name and version: $(grep PRETTY /etc/os-release)" #This will show os name and version
+echo "IP address = $(hostname -I)" #IPV4 Address
+echo "Root Filesystem Free Space: $(df -h --output=avail /dev/sda3 | grep '[0-8]')" #in this i have used grep to show the size only
+echo ====================================
