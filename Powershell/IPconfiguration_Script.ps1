a8a77b7a4b560f42e970f089b57b7b2bee1b481a
Mohit21032002 <105468700+Mohit21032002@users.noreply.github.com>
2022-07-28 16:10:22 -0400

    IP specs done
    done

--- Powershell/IPconfiguration_Script.ps1
+++ Powershell/IPconfiguration_Script.ps1
@@ -1,4 +1,4 @@
 get-ciminstance win32_networkadapterconfiguration |
 where { $_.ipenabled -eq "true" } |
-Format-Table Description, Index,
+Format-Table -autosize Description, Index,
 IPAddress, IPSubnet, DNSDomain, DNSServerSearchOrder
